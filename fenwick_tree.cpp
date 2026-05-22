f45e361d134e86025adf38c594266f2c1f6ec927
PertinaciousMedici <pertinaciouslymedici@gmail.com>
2026-05-22 22:43:19 +0000

    reformated and fixed an issue with the lambda passing
    lambdas have no default constructors and no copy assignment op when generated by the compiler
    using a member initialiser list bypasses this

--- fenwick_tree.cpp
+++ fenwick_tree.cpp
@@ -1,59 +1,60 @@
-struct FenwickTree {
-    vector<long long> bits;
-    long long sz = 0;
-    long long log2sz = 1;
+struct FenwickTree{
+	vector<long long> bits;
+	long long sz = 0;
+	long long log2sz = 1;
 
-    FenwickTree(long long n) {
-        this->sz = n;
-        this->bits.assign(this->sz + 1, 0);
-        while (this->log2sz < this->sz) this->log2sz <<= 1;
-    }
+	FenwickTree(long long n){
+		this->sz = n;
+		this->bits.assign(this->sz+1, 0);
+		while (this->log2sz<this->sz) this->log2sz <<= 1;
+	}
 
-    FenwickTree(const vector<long long>& a) {
-        this->sz = (long long) a.size();
-        this->bits.assign(this->sz + 1, 0);
-        for (long long i = 1; i <= this->sz; i++) {
-            this->bits[i] += a[i - 1];
-            long long j = i + (i & -i);
-            if (j <= this->sz) this->bits[j] += this->bits[i];
-        }
-        while (this->log2sz < this->sz) this->log2sz <<= 1;
-    }
+	FenwickTree(const vector<long long> &a){
+		this->sz = (long long)a.size();
+		this->bits.assign(this->sz+1, 0);
+		for (long long i = 1; i<=this->sz; i++){
+			this->bits[i] += a[i-1];
+			long long j = i+(i&-i);
+			if (j<=this->sz) this->bits[j] += this->bits[i];
+		}
+		while (this->log2sz<this->sz) this->log2sz <<= 1;
+	}
 
-    long long query(long long i) const {
-        i++;
-        long long ans = 0;
-        while (i > 0) {
-            ans += this->bits[i];
-            i -= (i & -i);
-        }
-        return ans;
-    }
+	long long query(long long i) const{
+		i++;
+		long long ans = 0;
+		while (i>0){
+			ans += this->bits[i];
+			i -= (i&-i);
+		}
+		return ans;
+	}
 
-    long long query(long long l, long long r) const {
-        return this->query(r) - this->query(l - 1);
-    }
+	long long query(long long l, long long r) const{
+		return this->query(r)-this->query(l-1);
+	}
 
-    void update(long long i, long long add) {
-        i++;
-        while (i <= this->sz) {
-            this->bits[i] += add;
-            i += (i & -i);
-        }
-    }
+	void update(long long i, long long add){
+		i++;
+		while (i<=this->sz){
+			this->bits[i] += add;
+			i += (i&-i);
+		}
+	}
 
-    void set(long long i, long long val) {
-        this->update(i, val - this->query(i, i));
-    }
+	void set(long long i, long long val){
+		this->update(i, val-this->query(i, i));
+	}
 
-    long long kth(long long k) const {
-        long long pos = 0;
-        for (long long pw = this->log2sz; pw > 0; pw >>= 1) {
-            if (pos + pw <= this->sz && this->bits[pos + pw] < k) {
-                pos += pw;
-                k -= this->bits[pos];
-            }
-        }
-        return pos;
-    }
+	long long kth(long long k) const{
+		long long pos = 0;
+		for (long long pw = this->log2sz; pw>0; pw >>= 1){
+			if (pos+pw<=this->sz&&this->bits[pos+pw]<k){
+				pos += pw;
+				k -= this->bits[pos];
+			}
+		}
+		return pos;
+	}
 };
+
--- segtree.cpp
+++ segtree.cpp
@@ -1,59 +1,55 @@
 template<typename T, typename F>
-struct SegTree {
-    int n;
-    vector<T> t;
-    T neutral;
-    F combine;
+struct SegTree{
+	int n;
+	vector<T> t;
+	T neutral;
+	F combine;
 
-    SegTree(int _n, T _neutral, F _combine) {
-        this->n = _n;
-        this->neutral = _neutral;
-        this->combine = _combine;
-        this->t.assign(4 * this->n, this->neutral);
-    }
+	SegTree(int _n, T _neutral, F _combine)
+		: n(_n), neutral(_neutral), combine(_combine){
+		t.assign(4*n, neutral);
+	}
 
-    SegTree(const vector<T>& _a, T _neutral, F _combine) {
-        this->n = (int) _a.size(); // narrowing conversion, explicit type cast better
-        this->neutral = _neutral;
-        this->combine = _combine;
-        this->t.assign(4 * this->n, this->neutral);
-        this->build(1, 0, this->n - 1, _a);
-    }
+	SegTree(const vector<T> &_a, T _neutral, F _combine)
+		: n((int)_a.size()), neutral(_neutral), combine(_combine){
+		t.assign(4*n, neutral);
+		build(1, 0, n-1, _a);
+	}
+	void build(int pos, int tl, int tr, const vector<T> &a){
+		if (tl==tr){
+			this->t[pos] = a[tl];
+			return;
+		}
+		int tm = tl+(tr-tl)/2;
+		this->build(2*pos, tl, tm, a);
+		this->build(2*pos+1, tm+1, tr, a);
+		this->t[pos] = this->combine(this->t[2*pos], this->t[2*pos+1]);
+	}
 
-    void build(int pos, int tl, int tr, const vector<T>& a) {
-        if (tl == tr) {
-            this->t[pos] = a[tl];
-            return;
-        }
-        int tm = tl + (tr - tl) / 2;
-        this->build(2 * pos, tl, tm, a);
-        this->build(2 * pos + 1, tm + 1, tr, a);
-        this->t[pos] = this->combine(this->t[2 * pos], this->t[2 * pos + 1]);
-    }
+	void update(int i, T val, int pos, int tl, int tr){
+		if (tl==tr){
+			this->t[pos] = val;
+			return;
+		}
+		int tm = tl+(tr-tl)/2;
+		if (i<=tm) this->update(i, val, 2*pos, tl, tm);
+		else this->update(i, val, 2*pos+1, tm+1, tr);
+		this->t[pos] = this->combine(this->t[2*pos], this->t[2*pos+1]);
+	}
 
-    void update(int i, T val, int pos, int tl, int tr) {
-        if (tl == tr) {
-            this->t[pos] = val;
-            return;
-        }
-        int tm = tl + (tr - tl) / 2;
-        if (i <= tm) this->update(i, val, 2 * pos, tl, tm);
-        else this->update(i, val, 2 * pos + 1, tm + 1, tr);
-        this->t[pos] = this->combine(this->t[2 * pos], this->t[2 * pos + 1]);
-    }
+	void update(int i, T val){
+		this->update(i, val, 1, 0, this->n-1);
+	}
 
-    void update(int i, T val) {
-        this->update(i, val, 1, 0, this->n - 1);
-    }
+	T query(int l, int r, int pos, int tl, int tr){
+		if (r<tl||tr<l) return this->neutral;
+		if (l<=tl&&tr<=r) return this->t[pos];
+		int tm = tl+(tr-tl)/2;
+		return this->combine(this->query(l, r, 2*pos, tl, tm), this->query(l, r, 2*pos+1, tm+1, tr));
+	}
 
-    T query(int l, int r, int pos, int tl, int tr) {
-        if (r < tl || tr < l) return this->neutral;
-        if (l <= tl && tr <= r) return this->t[pos];
-        int tm = tl + (tr - tl) / 2;
-        return this->combine(this->query(l, r, 2 * pos, tl, tm), this->query(l, r, 2 * pos + 1, tm + 1, tr));
-    }
-
-    T query(int l, int r) {
-        return this->query(l, r, 1, 0, this->n - 1);
-    }
+	T query(int l, int r){
+		return this->query(l, r, 1, 0, this->n-1);
+	}
 };
+
