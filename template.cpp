bf0b2f7e64e1afcf4019bc53de8617ba52c9c412
PertinaciousMedici <pertinaciouslymedici@gmail.com>
2026-05-22 20:34:33 +0000

    refactor template

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define ull unsigned long long
+
+#define upto(var, start, end) for(auto var = start; var < end; var++)
+#define downto(var, start, end) for(auto var = start; var > end; var--)
+#define foreach(element, collection) for(auto& element : collection)
+#define loop while(true)
+
+#define all(x) (x).begin(), (x).end()
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define mii map<int, int>
+#define mll map<ll, ll>
+#define umii unordered_map<int, int>
+#define umll unordered_map<ll, ll>
+
+#define pb push_back
+#define f first
+#define s second
+#define rs resize
+
+#define fast_io() ios::sync_with_stdio(false); cin.tie(nullptr);
+#define dbg(x) cerr << #x << " = " << x << "\n"
+
+#define endl "\n"
+
+using namespace std;
+
+int main(){
+	fast_io();
+}
