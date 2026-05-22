d2cd371ced05e35438d5ed51d7188c880d2a8a37
PertinaciousMedici <pertinaciouslymedici@gmail.com>
2026-05-22 21:10:55 +0000

    add inf && -inf

--- template.cpp
+++ template.cpp
@@ -23,6 +23,9 @@
 #define s second
 #define rs resize
 
+#define INF 0x3f3f3f3f;
+#define NINF -0x3f3f3f3f
+
 #define fast_io() ios::sync_with_stdio(false); cin.tie(nullptr);
 #define dbg(x) cerr << #x << " = " << x << "\n"
 
