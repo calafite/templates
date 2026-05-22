694ed56d613f48a19a037f1441ae97f75f96a21e
PertinaciousMedici <pertinaciouslymedici@gmail.com>
2026-05-22 20:28:02 +0000

    minor changes

--- fenwick_tree.cpp
+++ fenwick_tree.cpp
@@ -1,28 +1,28 @@
 struct FenwickTree {
-    vll bits;
-    ll sz = 0;
-    ll log2sz = 1;
+    vector<long long> bits;
+    long long sz = 0;
+    long long log2sz = 1;
 
-    FenwickTree(ll n) {
+    FenwickTree(long long n) {
         this->sz = n;
         this->bits.assign(this->sz + 1, 0);
         while (this->log2sz < this->sz) this->log2sz <<= 1;
     }
 
-    FenwickTree(vll const& a) {
-        this->sz = (ll) a.size();
+    FenwickTree(const vector<long long>& a) {
+        this->sz = (long long) a.size();
         this->bits.assign(this->sz + 1, 0);
-        for (ll i = 1; i <= this->sz; i++) {
+        for (long long i = 1; i <= this->sz; i++) {
             this->bits[i] += a[i - 1];
-            ll j = i + (i & -i);
+            long long j = i + (i & -i);
             if (j <= this->sz) this->bits[j] += this->bits[i];
         }
         while (this->log2sz < this->sz) this->log2sz <<= 1;
     }
 
-    ll query(ll i) {
+    long long query(long long i) const {
         i++;
-        ll ans = 0;
+        long long ans = 0;
         while (i > 0) {
             ans += this->bits[i];
             i -= (i & -i);
@@ -30,11 +30,11 @@ struct FenwickTree {
         return ans;
     }
 
-    ll query(ll l, ll r) {
+    long long query(long long l, long long r) const {
         return this->query(r) - this->query(l - 1);
     }
 
-    void update(ll i, ll add) {
+    void update(long long i, long long add) {
         i++;
         while (i <= this->sz) {
             this->bits[i] += add;
@@ -42,13 +42,13 @@ struct FenwickTree {
         }
     }
 
-    void set(ll i, ll val) {
+    void set(long long i, long long val) {
         this->update(i, val - this->query(i, i));
     }
 
-    ll kth(ll k) {
-        ll pos = 0;
-        for (ll pw = this->log2sz; pw > 0; pw >>= 1) {
+    long long kth(long long k) const {
+        long long pos = 0;
+        for (long long pw = this->log2sz; pw > 0; pw >>= 1) {
             if (pos + pw <= this->sz && this->bits[pos + pw] < k) {
                 pos += pw;
                 k -= this->bits[pos];
